763c5d5fe1288cae4fc270a6baab76043670b8cb
hollymsnell <hollymsnell@gmail.com>
2023-10-13 20:50:14 -0600

    updated image links for seeded photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-Photo.create(name: "Winter", location: "Iceland", width: 200, height: 150)
-Photo.create(name: "Family", location: "West Virginia", width: 1024, height: 768)
-Photo.create(name:"Pets", location: "Home", width: 200, height: 150)
+Photo.create(name: "Winter", location: "Iceland", width: 200, height: 150, image: "https://cdn.kimkim.com/files/a/content_articles/featured_photos/f2d66254ca2fb9cf82d08a3885e21b5d3bc402c2/big-7b68f27a1fb951bf3dba545e90ea4ab9.jpg")
+Photo.create(name: "Family", location: "West Virginia", width: 1024, height: 768, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Grist-mill-creek-reflections.jpg/800px-Grist-mill-creek-reflections.jpg")
+Photo.create(name:"Pets", location: "Home", width: 200, height: 150, image: "https://i.guim.co.uk/img/media/03734ee186eba543fb3d0e35db2a90a14a5d79e3/0_173_5200_3120/master/5200.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=9c30ed97ea8731f3e2a155467201afe3")
